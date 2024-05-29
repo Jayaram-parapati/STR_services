01447ee0eea05f6e5bd8dfa777284c3670a313c0
Jayaram-parapati <140514791+Jayaram-parapati@users.noreply.github.com>
2024-05-29 10:54:42 +0530

    cors issue

--- server.py
+++ server.py
@@ -29,15 +29,15 @@ db = db_connection.db
 
 
 app = FastAPI(title='STR Services')
-# origins = [
-#     "https://localhost:8000",  
-#     "https://localhost:7187",
-# ]
+origins = [
+    "https://localhost:8000",  
+    "https://localhost:7187",
+]
 
 app.add_middleware(
     CORSMiddleware,
-    # allow_origins=origins,
-    allow_origins=["*"],
+    allow_origins=origins,
+    # allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
